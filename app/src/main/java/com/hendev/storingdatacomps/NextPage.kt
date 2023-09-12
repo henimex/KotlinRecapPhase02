9b127e982c48cd940cc0a302c484b8b78385a291
henimex <49072861+henimex@users.noreply.github.com>
2023-09-12 11:38:23 +0300

    Lifecycle

--- app/src/main/java/com/hendev/storingdatacomps/NextPage.kt
+++ app/src/main/java/com/hendev/storingdatacomps/NextPage.kt
@@ -1,7 +1,9 @@
 package com.hendev.storingdatacomps
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.hendev.storingdatacomps.databinding.ActivityNextPageBinding
 
 class NextPage : AppCompatActivity() {
@@ -20,5 +22,11 @@ class NextPage : AppCompatActivity() {
         } else {
             binding.txtSoredAge.text = "? Age Did Not Received"
         }
+    }
+
+    fun goBack(view: View) {
+        val intent = Intent(this,MainActivity::class.java)
+        startActivity(intent);
+        finish();
     }
 }

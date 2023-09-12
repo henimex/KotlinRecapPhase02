ea6d82799b120ee76c27ff09e46c7e5612872818
henimex <49072861+henimex@users.noreply.github.com>
2023-09-12 13:56:35 +0300

    Countdown Timer

--- app/src/main/java/com/hendev/storingdatacomps/MainActivity.kt
+++ app/src/main/java/com/hendev/storingdatacomps/MainActivity.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -24,6 +25,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var buttonTest: Button;
     private lateinit var txtAge: EditText;
     private lateinit var txtResult: TextView;
+    private lateinit var txtTimer: TextView;
 
     private var ageFromPref: Int? = null;
 
@@ -33,6 +35,21 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
         assignBindings();
+        countDownOperations();
+    }
+
+    private fun countDownOperations() {
+        object : CountDownTimer(10000, 1000) {
+            override fun onTick(p0: Long) {
+                println("ss")
+                txtTimer.text = "${p0 / 1000}";
+            }
+
+            override fun onFinish() {
+                txtTimer.text = "Countdown Finished";
+            }
+        }.start();
+
     }
 
     fun methodTests(view: View) {
@@ -41,7 +58,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun goToNextPage() {
         val intent = Intent(this@MainActivity, NextPage::class.java)
-        intent.putExtra("age_data",ageFromPref);
+        intent.putExtra("age_data", ageFromPref);
         startActivity(intent);
     }
 
@@ -94,6 +111,7 @@ class MainActivity : AppCompatActivity() {
         buttonTest = binding.btnTest
         txtAge = binding.txtAge;
         txtResult = binding.txtResult;
+        txtTimer = binding.txtCountDown;
 
         sharedPref = this.getSharedPreferences("henimex.storage.data", Context.MODE_PRIVATE)
         ageFromPref = this.sharedPref.getInt("age_data", -1)
@@ -104,5 +122,4 @@ class MainActivity : AppCompatActivity() {
             this.txtResult.text = "Your Age is $ageFromPref";
         }
     }
-
 }
